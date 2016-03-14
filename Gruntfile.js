a1508b33a43fbb2f49b42fad5cf9b3ca1c1afb5d
rod7val <rod7val@gmail.com>
2016-03-14 20:28:16 -0300

    eliminar tarea 'buil' de Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,12 @@
 
 module.exports = function(grunt) {
+
   // cargar tareas npm
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-express-server');
-  grunt.loadNpmTasks('grunt-shared-config');
 
   grunt.initConfig({
-    // mi npm json
-    pkg: grunt.file.readJSON('package.json'),
-    // compilar arcgivo config.json (bootstrap) en compile/css/varibles.less
-    shared_config: {
-      default: {
-        src: 'compile/config.json',
-        dest: ['compile/css/variables.less']
-      }
-    },
-    // less
-    less: {
-      development: {
-        options: { compress: true },
-        files: {
-          'public/stylesheets/frontend.css':'compile/css/frontend.less',
-          'public/stylesheets/backend.css':'compile/css/backend.less'
-        }
-      }
-    },
-    // compilador
-    uglify: {
-      options: { mangle: false },
-      frontend: {
-        files: {
-          'public/javascripts/frontend.js': [  // frontend js
-            'compile/jquery/dist/jquery.min.js',
-            'compile/bootstrap/dist/js/bootstrap.min.js',
-            'compile/js/frontend.js',
-            'compile/js/mapbox.js'
-          ]
-        }
-      },
-      backend: {
-        files: {
-          'public/javascripts/backend.js': [   // backend js
-            'compile/jquery/dist/jquery.min.js',
-            'compile/bootstrap/dist/js/bootstrap.min.js',
-            // 'compile/js/backend.js'
-          ]
-        }
-      }
-    },
+
     // inspeccion de archivos en tiempo real
     watch: {
       express: {
@@ -74,16 +30,16 @@ module.exports = function(grunt) {
         options: { livereload: true, spawn: false }
       }
     },
+
     // servidor
     express: {
       options: { script: 'bin/www', port: 3000 },
       defaults: {}
     }
-  })
-  ;
+
+  });
 
   // registrar tareas grunt
   grunt.registerTask('default', ['express:defaults', 'watch']);
-  grunt.registerTask('build', ['uglify', 'shared_config', 'less'])
 
 };
